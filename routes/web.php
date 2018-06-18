cf74e2842e3996b5623f3bb5b106a7907d45a012
Christos Sarantis <i@sarantis.pw>
2018-06-18 09:38:08 +0300

    routes refactoring

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//auth
 Auth::routes();
-
-//Route::get('/', 'WelcomeController@index')->name('home');
-Route::get('/about', function () {
-    return view('about');
-});
+//index
 Route::get('language/{lang}', 'HomeController@language')->where('lang', '[A-Za-z_-]+');
 Route::get('/', 'HomeController@index')->name('index');
-
-Route::get('/schedule', function () {
-    return view('calendar');
-});
-
 Route::get('/contact', 'ContactController@create')->name('contact');
 Route::post('/contact', 'ContactController@store')->name('contact_store');
 Route::get('/menu', 'HomeController@menu')->name('menu');
@@ -31,17 +23,21 @@ Route::post('/feedback', 'FeedbackController@store')->name('feedback_store');
 
 
 //students profile
+//todo create controller and view index
 Route::group(['prefix' => 'student', 'middleware' => 'can_view_student'], function () {
     Route::get('{studentId}/profile', 'StudentsController@profile')->name('profile')->middleware('student_exists');
-    Route::get('{studentId}/reviews', 'StudentsController@reviews')->name('reviews')->middleware('student_exists');
+//    Route::get('{studentId}/reviews', 'StudentsController@reviews')->name('reviews')->middleware('student_exists');
 });
 
-Route::get('/dashboard', function () {
-    return view('about');
-})->name('dashboard');
+//dashboard
+Route::get('/dashboard', 'DashboardController@dashboard')->name('dashboard');
+Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function () {
+    Route::get('about', 'DashboardController@about')->name('about');
+    Route::get('calendar', 'DashboardController@calendar')->name('calendar');
+});
 
 //admin
-Route::get('/admin', 'DashboardController@home')->name('admin');
+Route::get('/admin', 'DashboardController@admin')->name('admin');
 Route::get('/admin/meals', 'MealsController@index')->name('admin_meals');
 Route::post('/admin/meals', 'MealsController@post')->name('admin_meals');
 Route::post('/admin/meals/update', 'MealsController@update')->name('admin_meals_update');
@@ -59,4 +55,3 @@ Route::post('/admin/announcements/delete', 'AnnouncementsController@delete')->na
 Route::get('/admin/statistics', 'StatsController@index')->name('admin_statistics');
 Route::post('/admin/statistics', 'StatsController@search')->name('admin_statistics');
 Route::get('/admin/feedback', 'feedbackController@index')->name('admin_feedback');
-//Route::get('/admin', 'DashboardController@index')->name('admin');//->middleware('is_admin')
