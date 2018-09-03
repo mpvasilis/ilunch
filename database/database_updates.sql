b5960ee8c3106b8f182a61c9199a0667f76bb53d
Christos Sarantis <i@sarantis.pw>
2018-09-04 02:45:35 +0300

    picture default update

--- database/database_updates.sql
+++ database/database_updates.sql
@@ -8,3 +8,14 @@ UPDATE `settings` SET value = $example WHERE name = 'DATABASE_REVISION';
 ##############################################
 # END UPDATE FOR REVISION $example           #
 ##############################################
+
+##############################################
+# UPDATE FOR REVISION $example               #
+##############################################
+ALTER TABLE `students` CHANGE `photo` `photo` VARCHAR(75) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'profilePlaceholder.jpg';
+
+#update revision number $example
+UPDATE `settings` SET value = 3 WHERE name = 'DATABASE_REVISION';
+##############################################
+# END UPDATE FOR REVISION $example           #
+##############################################
